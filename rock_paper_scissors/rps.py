efe7221995e7f2f12b72122490e9a97525a2ada3
jamespagedev <jamespagedev@outlook.com>
2019-03-25 12:39:01 -0700

    rps - needs debugging

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,8 +4,38 @@ import sys
 
 
 def rock_paper_scissors(n):
+    # edge case n == 0, return [[]]
     if n == 0:
         return [[]]
+
+    # edge case n == 1, return [['rock'], ['paper'], ['scissors']]
+    if n == 1:
+        return [['rock'], ['paper'], ['scissors']]
+
+    # general case...
+
+    # Assign rock, paper, scissors to an array(rps)
+    rps = ['rock', 'paper', 'scissors']
+
+    # instantiate an empty array called possible_plays
+    possible_plays = []
+
+    # use a recursive helper method called get_unused_play
+    def get_unused_play(play, exponentNumber):
+        for hand in rps:
+            play.append(hand)
+            if exponentNumber == n:
+                possible_plays.append(play)
+            else:
+                get_unused_play(play, exponentNumber + 1)
+
+            # https://medium.com/@junshengpierre/recursion-rockpaperscissors-js-b071a334080a
+            # debug later, move onto next problem
+
+    # call recursive helper method
+    get_unused_play([], 1)
+
+    # return possible_plays
 
 
 if __name__ == "__main__":

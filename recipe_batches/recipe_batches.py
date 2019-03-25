5b0f76ccb62d4eeeb857d12ff12b2142da24a85f
jamespagedev <jamespagedev@outlook.com>
2019-03-25 13:00:00 -0700

    recipe_batches - 1st Test (PASS)

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,25 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # Edge case - check to see if ingredients dict has all of the keys from recipe dict
+    # if not, return 0
+    for key in recipe:
+        if key in ingredients.keys():
+            return 0
+
+    # General case...
+    pass
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    pass
+# print(recipe_batches({'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5}, {
+#     'milk': 1288, 'flour': 9, 'sugar': 95}))
